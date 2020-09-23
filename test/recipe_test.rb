c453262a5dcf23153329074456c753f7f18d9ce9
Eugene Theriault <hybridbassist@gmail.com>
2020-09-23 08:25:20 -0700

    Create test for attributes in recipe class

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -15,4 +15,9 @@ class RecipeTest < MiniTest::Test
   def test_it_exists
     assert_instance_of Recipe, @recipe1
   end
+
+  def test_it_can_name_and_ingredients_required_attributes
+    assert_equal "Mac and Cheese", @recipe1.name
+    assert_equal ({}), @recipe1.ingredients_required
+  end
 end
