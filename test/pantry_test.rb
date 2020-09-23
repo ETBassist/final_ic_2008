4b27139aa11452e4644f960bf5b0cfdc2578afc4
Eugene Theriault <hybridbassist@gmail.com>
2020-09-23 09:07:18 -0700

    Create test for enough ingredients method in pantry

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,12 +3,13 @@ require 'minitest/pride'
 
 require './lib/ingredient'
 require './lib/pantry'
+require './lib/recipe'
 
 class PantryTest < MiniTest::Test
   def setup
     @pantry = Pantry.new
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
   end
 
   def test_it_exists
@@ -26,5 +27,19 @@ class PantryTest < MiniTest::Test
     assert_equal 15, @pantry.stock_check(@ingredient1)
     @pantry.restock(@ingredient2, 7)
     assert_equal 7, @pantry.stock_check(@ingredient2)
+  end
+
+  def test_it_can_check_if_enough_ingredients_for_recipe
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    @pantry.restock(@ingredient2, 7)
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    @pantry.restock(@ingredient2, 1)
+    assert_equal true, @pantry.enough_ingredients_for?(@recipe1)
   end
 end
